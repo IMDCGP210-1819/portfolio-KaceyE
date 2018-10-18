f474b41ea18816224c9de6b8f01926c3932e0942
KaceyE <43639445+KaceyE@users.noreply.github.com>
2018-10-18 05:45:42 +0100

    Add files via upload

--- ex5,6,7.py
+++ ex5,6,7.py
@@ -49,37 +49,30 @@ while ans:
 
 
     elif menu == "3":
-    month_counter = 0
-    portion_deposit = 0.20
-    current_savings = 0
-    r = 0.04
+        month_counter = 0
+        portion_deposit = 0.20
+        current_savings = 0
+        r = 0.04
 
 
-    annual_salary = input ("Please enter your current annual salary: ")
-    portion_saved = input ("Please enter how much you would like to be saved, as a decimal: ")
-    total_cost = input ("Please enter the cost of your dream home: ")
+        annual_salary = input ("Please enter your current annual salary: ")
+        portion_saved = input ("Please enter how much you would like to be saved, as a decimal: ")
+        total_cost = input ("Please enter the cost of your dream home: ")
 
-    annual_salary = int(annual_salary)
-    portion_saved = float(portion_saved)
-    total_cost = float(total_cost)
+        annual_salary = int(annual_salary)
+        portion_saved = float(portion_saved)
+        total_cost = float(total_cost)
 
-    monthly_salary = annual_salary/12 
+        monthly_salary = annual_salary/12 
 
-    while month_counter != 12:
+        while month_counter != 12:
+
+            current_savings = monthly_salary + (monthly_salary/r)
+
+            if current_savings == total_cost:
 
 
-
-
-
-        total_cost = total_cost - (total_cost*portion_deposit)
-
-        monthly_salary = annual_salary/12
-
-        current_savings = monthly_salary*portion_saved
-
-        current_savings = current_savings*r
-
-        months = total_cost/current_savings
-
-        print (months)
-
+            elif:
+                month_counter = month_counter + 1
+                print (current_savings)
+                print (total_cost)
